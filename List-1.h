5972a56cafd792afa16a2b5a5603e95388dcb3b1
Daniel Martinez <danielmartinez900@mail.fresnostate.edu>
2021-10-30 01:31:47 -0700

    all functions done

--- List-1.h
+++ List-1.h
@@ -20,8 +20,49 @@ private:
 public:
 	BST();
 	Node* getRoot(){ return this->root; };
-	void printTree(Node *root);
-	Node* InsertNode(Node *root,int data);
-	bool FindNode(Node *root,int data);
-	int Largest(Node *root);
+	void printTree(Node* root)
+	{
+		if (root == nullptr) {
+			return; }
+		printTree(root->LeftChild);
+		printTree(root->RightChild);
+		cout << root->data << " ";
+
+
+	}
+	Node* InsertNode(Node* root, int data)
+	{
+		if (root == nullptr)
+		{
+			Node* nn = new Node(data);
+			root = nn;
+			return root;
+		}
+		if (data < root->data)
+		{
+			root->LeftChild = InsertNode(root->LeftChild, data);
+		}
+		else
+		{
+			root->RightChild = InsertNode(root->RightChild, data);
+		}
+		return root;
+	}
+	bool FindNode(Node* root, int data)
+	{
+		if (root == nullptr || root->data == data)
+			return root;
+		else if (root->data < data)
+			return FindNode(root->RightChild, data);
+		else if (root->data > data)
+			return FindNode(root->LeftChild, data);
+	}
+	int Largest(Node* root)
+	{
+		while (root->RightChild != nullptr)
+		{
+			root = root->RightChild;
+		}
+		return root->data;
+	}
 };
